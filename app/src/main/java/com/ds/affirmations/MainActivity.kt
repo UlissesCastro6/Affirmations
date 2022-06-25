0f43a7161b2979f76b5c0f9ae85ca0e7d56c6dde
Ulisses Castro <ulisses.castro@etec.sp.gov.br>
2022-06-25 00:10:37 -0300

    subindo atividade

---
+++ app/src/main/java/com/ds/affirmations/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.ds.affirmations
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.ds.affirmations.adapter.ItemAdapter
+import com.ds.affirmations.data.Datasource
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        /*val textView: TextView = findViewById (R.id.TextView)
+        textView.text = Datasource().loadAffirmations().size.toString()*/
+
+        val myDataset = Datasource().loadAffirmations()
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+        recyclerView.setHasFixedSize(true)
+
+
+    }
+}
+
+
+
+
